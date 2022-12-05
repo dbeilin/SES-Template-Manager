60a57facdba918dee656579bcf6c6275539d0b12
Daniel Beilin <beylin.daniel@gmail.com>
2022-12-05 20:35:12 +0200

    wip

--- app.py
+++ app.py
@@ -1,1 +1,23 @@
+import boto3
 import tkinter
+import customtkinter
+
+###### AWS ######
+ses = boto3.client('ses')
+
+###### Theme ######
+customtkinter.set_appearance_mode("System")
+customtkinter.set_default_color_theme("blue")
+
+###### Main ######
+app = customtkinter.CTk()
+app.geometry("1200x700")
+
+###### Widgets ######
+
+templates_list_widget = customtkinter.CTkOptionMenu(master=app)
+
+button = customtkinter.CTkButton(master=app, text="CTkButton")
+button.place(relx=0.5, rely=0.5, anchor=tkinter.CENTER)
+
+app.mainloop()
