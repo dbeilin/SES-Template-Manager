d14ac7e6c5b0207de9ea49e4745faea49956309c
Daniel Beilin <beylin.daniel@gmail.com>
2022-12-15 16:39:16 +0200

    switch to classes

--- app.py
+++ app.py
@@ -1,71 +1,71 @@
 import boto3
-from tkinter import *
+# from tkinter import *
 import customtkinter
+
+customtkinter.set_appearance_mode("Dark")  # Modes: "System" (standard), "Dark", "Light"
+customtkinter.set_default_color_theme("blue")  # Themes: "blue" (standard), "green", "dark-blue"
 
 ###### AWS ######
 ses = boto3.client('ses')
 
-###### TK ######
-app = customtkinter.CTk()
-app.geometry("1200x700")
-# app.maxsize(1200, 700)
+class App(customtkinter.CTk):
+    def __init__(self):
+        super().__init__()
 
-###### Functions ######
-def get_templates():
-    templates_list = []
-    response = ses.list_templates()
-    for template in response['TemplatesMetadata']:
-        templates_list.append(template['Name'])
+        # configure window
+        self.title("SES Template Manager")
+        self.geometry(f"{1200}x{700}")
 
-    return templates_list
+        # Top frame
+        self.top_menu_frame = customtkinter.CTkFrame(master=self, height=40)
+        self.top_menu_frame.grid(row=0, column=1, padx=5, pady=5, sticky="news")
 
-###### Tkinter ######
-# Top frame
-top_menu_frame = customtkinter.CTkFrame(master=app, height=40)
-top_menu_frame.grid(row=0, column=1, padx=5, pady=5, sticky="news")
+        self.aws_id = customtkinter.CTkLabel(master=self.top_menu_frame, text="AWS Access Key ID")
+        self.aws_id.grid(row=0, column=0, padx=5, pady=5)
+        self.aws_id_entry = customtkinter.CTkEntry(self.top_menu_frame, width=150, height=35)
+        self.aws_id_entry.grid(row=0, column=1, padx=5, pady=5)
 
-aws_id = customtkinter.CTkLabel(master=top_menu_frame, text="AWS Access Key ID")
-aws_id.grid(row=0, column=0, padx=5, pady=5)
-aws_id_entry = customtkinter.CTkEntry(top_menu_frame, width=150, height=35)
-aws_id_entry.grid(row=0, column=1, padx=5, pady=5)
+        self.aws_key = customtkinter.CTkLabel(master=self.top_menu_frame, text="AWS Secret Access Key")
+        self.aws_key.grid(row=0, column=2, padx=5, pady=5)
+        self.aws_key_entry = customtkinter.CTkEntry(self.top_menu_frame, width=150, height=35, show="*")
+        self.aws_key_entry.grid(row=0, column=3, padx=5, pady=5)
 
-aws_key = customtkinter.CTkLabel(master=top_menu_frame, text="AWS Secret Access Key")
-aws_key.grid(row=0, column=2, padx=5, pady=5)
-aws_key_entry = customtkinter.CTkEntry(top_menu_frame, width=150, height=35, show="*")
-aws_key_entry.grid(row=0, column=3, padx=5, pady=5)
+        self.aws_region = customtkinter.CTkLabel(master=self.top_menu_frame, text="Region")
+        self.aws_region.grid(row=0, column=4, padx=5, pady=5)
+        self.aws_region_entry = customtkinter.CTkEntry(self.top_menu_frame, width=150, height=35)
+        self.aws_region_entry.grid(row=0, column=5, padx=5, pady=5)
 
-aws_region = customtkinter.CTkLabel(master=top_menu_frame, text="Region")
-aws_region.grid(row=0, column=4, padx=5, pady=5)
-aws_region_entry = customtkinter.CTkEntry(top_menu_frame, width=150, height=35)
-aws_region_entry.grid(row=0, column=5, padx=5, pady=5)
+        self.load_aws_creds = customtkinter.CTkButton(master=self.top_menu_frame, text="Load")
+        self.load_aws_creds.grid(row=0, column=6, padx=5, pady=5)
 
-load_aws_creds = customtkinter.CTkButton(master=top_menu_frame, text="Load")
-load_aws_creds.grid(row=0, column=6, padx=5, pady=5)
+        # Left frame
+        self.left_menu_frame = customtkinter.CTkFrame(master=self, width=300)
+        self.left_menu_frame.grid(row=1, column=0, padx=5, pady=5, sticky="news")
 
-# Left frame
-left_menu_frame = customtkinter.CTkFrame(master=app, width=300)
-left_menu_frame.grid(row=1, column=0, padx=5, pady=5, sticky="news")
+        self.get_templates_button = customtkinter.CTkButton(master=self.left_menu_frame, text="Get Templates")
+        self.get_templates_button.grid(row=0, column=0, padx=5, pady=5)
 
-get_templates_button = customtkinter.CTkButton(master=left_menu_frame, text="Get Templates")
-get_templates_button.grid(row=0, column=0, padx=5, pady=5)
+        self.update_template_button = customtkinter.CTkButton(master=self.left_menu_frame, text="Update Template")
+        self.update_template_button.grid(row=1, column=0, padx=5, pady=5)
 
-update_template_button = customtkinter.CTkButton(master=left_menu_frame, text="Update Template")
-update_template_button.grid(row=1, column=0, padx=5, pady=5)
+        self.create_template_button = customtkinter.CTkButton(master=self.left_menu_frame, text="Create Template")
+        self.create_template_button.grid(row=2, column=0, padx=5, pady=5)
 
-create_template_button = customtkinter.CTkButton(master=left_menu_frame, text="Create Template")
-create_template_button.grid(row=2, column=0, padx=5, pady=5)
+        self.load_template_button = customtkinter.CTkButton(master=self.left_menu_frame, text="Load Template")
+        self.load_template_button.grid(row=3, column=0, padx=5, pady=5)
 
-load_template_button = customtkinter.CTkButton(master=left_menu_frame, text="Load Template", command=get_templates)
-load_template_button.grid(row=3, column=0, padx=5, pady=5)
+        self.templates_list_optionmenu = customtkinter.CTkComboBox(master=self.left_menu_frame)
+        self.templates_list_optionmenu.grid(row=4, column=0, padx=5, pady=5)
 
-templates_list_optionmenu = customtkinter.CTkComboBox(master=left_menu_frame)
-templates_list_optionmenu.grid(row=4, column=0, padx=5, pady=5)
+        # Right frame
+        self.text_box_frame = customtkinter.CTkFrame(master=self, width=800)
+        self.text_box_frame.grid(row=1, column=1, padx=5, pady=5, sticky="news")
 
-# Right frame
-text_box_frame = customtkinter.CTkFrame(master=app, width=800)
-text_box_frame.grid(row=1, column=1, padx=5, pady=5, sticky="news")
+        self.template_text = customtkinter.CTkTextbox(self.text_box_frame, width=1000, height=600)
+        self.template_text.pack(expand=True, fill='both')
 
-template_text = customtkinter.CTkTextbox(text_box_frame, width=1000, height=600)
-template_text.pack(expand=True, fill='both')
 
-app.mainloop()
+
+if __name__ == "__main__":
+    app = App()
+    app.mainloop()
---
+++ old_app.py
@@ -0,0 +1,72 @@
+import boto3
+from tkinter import *
+import customtkinter
+
+###### AWS ######
+ses = boto3.client('ses')
+
+###### App ######
+app = customtkinter.CTk()
+app.maxsize(1200, 700)
+app.minsize(1200, 700)
+
+###### Functions ######
+def get_templates():
+    templates_list = []
+    response = ses.list_templates()
+    for template in response['TemplatesMetadata']:
+        templates_list.append(template['Name'])
+
+    return templates_list
+
+###### Tk ######
+# Top frame
+top_menu_frame = customtkinter.CTkFrame(master=app, height=40)
+top_menu_frame.grid(row=0, column=1, padx=5, pady=5, sticky="news")
+
+aws_id = customtkinter.CTkLabel(master=top_menu_frame, text="AWS Access Key ID")
+aws_id.grid(row=0, column=0, padx=5, pady=5)
+aws_id_entry = customtkinter.CTkEntry(top_menu_frame, width=150, height=35)
+aws_id_entry.grid(row=0, column=1, padx=5, pady=5)
+
+aws_key = customtkinter.CTkLabel(master=top_menu_frame, text="AWS Secret Access Key")
+aws_key.grid(row=0, column=2, padx=5, pady=5)
+aws_key_entry = customtkinter.CTkEntry(top_menu_frame, width=150, height=35, show="*")
+aws_key_entry.grid(row=0, column=3, padx=5, pady=5)
+
+aws_region = customtkinter.CTkLabel(master=top_menu_frame, text="Region")
+aws_region.grid(row=0, column=4, padx=5, pady=5)
+aws_region_entry = customtkinter.CTkEntry(top_menu_frame, width=150, height=35)
+aws_region_entry.grid(row=0, column=5, padx=5, pady=5)
+
+load_aws_creds = customtkinter.CTkButton(master=top_menu_frame, text="Load")
+load_aws_creds.grid(row=0, column=6, padx=5, pady=5)
+
+# Left frame
+left_menu_frame = customtkinter.CTkFrame(master=app, width=300)
+left_menu_frame.grid(row=1, column=0, padx=5, pady=5, sticky="news")
+
+get_templates_button = customtkinter.CTkButton(master=left_menu_frame, text="Get Templates")
+get_templates_button.grid(row=0, column=0, padx=5, pady=5)
+
+update_template_button = customtkinter.CTkButton(master=left_menu_frame, text="Update Template")
+update_template_button.grid(row=1, column=0, padx=5, pady=5)
+
+create_template_button = customtkinter.CTkButton(master=left_menu_frame, text="Create Template")
+create_template_button.grid(row=2, column=0, padx=5, pady=5)
+
+load_template_button = customtkinter.CTkButton(master=left_menu_frame, text="Load Template", command=get_templates)
+load_template_button.grid(row=3, column=0, padx=5, pady=5)
+
+templates_list_optionmenu = customtkinter.CTkComboBox(master=left_menu_frame)
+templates_list_optionmenu.grid(row=4, column=0, padx=5, pady=5)
+
+# Right frame
+text_box_frame = customtkinter.CTkFrame(master=app, width=800)
+text_box_frame.grid(row=1, column=1, padx=5, pady=5, sticky="news")
+
+template_text = customtkinter.CTkTextbox(text_box_frame, width=1000, height=600)
+template_text.pack(expand=True, fill='both')
+
+if __name__ == "__main__":
+    app.mainloop()
